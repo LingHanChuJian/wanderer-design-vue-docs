83f80dd18284ab9a674d6b1826f366a5a4452cc1
Linghanchujian <2739069093@qq.com>
2022-07-21 22:20:26 +0800

    update: gitignore

--- docs/.vuepress/.temp/internal/layoutComponents.js
+++
@@ -1,6 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const layoutComponents = {
-  "404": defineAsyncComponent(() => import("C:/Users/linghanchujian/Desktop/wanderer-design-vue-docs/theme/layouts/404.vue")),
-  "Layout": defineAsyncComponent(() => import("C:/Users/linghanchujian/Desktop/wanderer-design-vue-docs/theme/layouts/Layout.vue")),
-}
